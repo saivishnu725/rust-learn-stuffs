c6b0224091904db6426e7e363c07d9d30a440a19
Sai Vishnu <saivishnu725@gmail.com>
2021-07-26 21:53:41 +0530

    Some variables and tuples stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: u32 = 5;
+    println!("value = {}", x);
+    x = 10;
+    println!("value = {}", x);
+    let tup: (u32, f64, bool) = (200, 20.0, true);
+    let (a, b, _c) = tup;
+    println!("A = {}", a);
+    println!("B = {}", b);
+    println!("C = {}", tup.2);
 }
